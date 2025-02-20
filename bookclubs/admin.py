4c4725caba068841809e5c041676a2fbc54e5f5c
angelogentileiii <angelo.john.gentile@gmail.com>
2025-02-20 12:12:49 -0500

    bookclubs listview and template

--- bookclubs/admin.py
+++ bookclubs/admin.py
@@ -5,16 +5,33 @@ from .models import BookClub, PastBook, Membership
 # Register your models here.
 @admin.register(BookClub)
 class BookClubAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "current_book", "get_members", "visibility", "creator")
+    list_display = (
+        "__str__",
+        "current_book",
+        "get_members",
+        "visibility",
+        "creator",
+        "get_past_books",
+    )
     list_filter = ("name",)
 
     def get_members(self, obj):
         members = obj.members.all()
         return ", ".join(str(member.user) for member in members)
 
+    def get_past_books(self, obj):
+        past_books = obj.past_books.all()
+        return ", ".join(str(past_book.book) for past_book in past_books)
+
     get_members.short_description = "Members"
+    get_past_books.short_description = "Past Books"
 
     @admin.register(Membership)
     class MembershipAdmin(admin.ModelAdmin):
         list_display = ("user", "bookclub", "role")
         list_filter = ("role", "bookclub")
+
+    @admin.register(PastBook)
+    class PastBookAdmin(admin.ModelAdmin):
+        list_display = ("book", "bookclub")
+        list_filter = ("bookclub", "book")
--- bookclubs/views.py
+++ bookclubs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import BookClub
+
 
 # Create your views here.
+class BookClubsView(ListView):
+    model = BookClub
+    template_name = "bookclubs/bookclubs.html"
+    context_object_name = "bookclubs"
+    ordering = ["name"]
